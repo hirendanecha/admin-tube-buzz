7530bb335780f8a3db35b59fd8c2542d7f6bbc98
vatsal <vso.opash@gmail.com>
2024-06-19 18:36:22 +0530

    initial commit with name and logo change

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: 'https://api.tube.buzz/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
+  socketUrl: 'https://api.tube.buzz/',
+  socketUrl2: 'http://localhost:8080/',
+  imgUrl: 'root/storage/uploads',
+};
